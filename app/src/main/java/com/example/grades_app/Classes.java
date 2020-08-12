b40683cb08d9e7602154eb61127f03df352e37e8
Ashok Meyyappan <ashoksteelers12@gmail.com>
2020-08-12 15:28:55 -0500

    updated

---
+++ app/src/main/java/com/example/grades_app/Classes.java
@@ -0,0 +1,35 @@
+package com.example.grades_app;
+
+public class Classes {
+    private String name;
+    private Float grade;
+
+    public Classes(String name, Float grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Float grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Classes{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
