a487d7c08d14656cc7540f41a1c1422b6e1d4b40
megamp15 <47288803+megamp15@users.noreply.github.com>
2020-08-12 14:00:35 -0500

    Created recyclerview of classes

--- app/src/main/java/com/example/grades_app/MainActivity.java
+++ app/src/main/java/com/example/grades_app/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.grades_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView classes_recView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        classes_recView = findViewById(R.id.classes_recView);
+
+        ArrayList<Classes> classes = new ArrayList<>();
+        classes.add(new Classes("Test Class 1", (float) 95.2));
+        classes.add(new Classes("Test Class 2", (float) 87.9));
+        classes.add(new Classes("Test Class 3", (float) 83.4));
+        classes.add(new Classes("Test Class 4", (float) 92.9));
+        classes.add(new Classes("Test Class 5", (float) 79.1));
+
+        ClassesRecViewAdapter adapter = new ClassesRecViewAdapter(this);
+        adapter.setClasses(classes);
+
+        classes_recView.setAdapter(adapter);
+        classes_recView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
