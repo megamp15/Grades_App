8e90c2664045521af6a6d1d47181585a01eaa7ce
Ashok Meyyappan <61124747+ashoksteelers12@users.noreply.github.com>
2020-08-17 23:11:41 -0500

    updated

--- app/src/main/java/com/example/grades_app/Calendar.java
+++ app/src/main/java/com/example/grades_app/Calendar.java
@@ -5,16 +5,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 public class Calendar extends AppCompatActivity {
+    EditText name;
+    EditText description;
+    Button addReminder;
+    Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        name = findViewById(R.id.calendar_name);
+        description = findViewById(R.id.calendar_description);
+        addReminder = findViewById(R.id.calendar_button);
+        nextPage = findViewById(R.id.calendar_next_page);
+
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ViewEvents.class));
+            }
+        });
+
+        addReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!name.getText().toString().isEmpty() && !description.getText().toString().isEmpty()) {
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.TITLE, name.getText().toString());
+                    intent.putExtra(CalendarContract.Events.DESCRIPTION, description.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+
+
+                    if(intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(Calendar.this, "No app", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(Calendar.this, "Fill in all the blanks", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
--- app/src/main/java/com/example/grades_app/MainActivity.java
+++ app/src/main/java/com/example/grades_app/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.grades_app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,10 +18,39 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView classes_recView;
+    private Button btn_terms;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //      Terms
+        btn_terms = findViewById(R.id.btn_terms);
+        btn_terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), TermsActivity.class));
+                overridePendingTransition(0, 0);
+            }
+        });
+
+//      Recycler for Classes
+        classes_recView = findViewById(R.id.classes_recView);
+
+        ArrayList<Classes> classes = new ArrayList<>();
+        classes.add(new Classes("Test Class 1", (float) 95.2));
+        classes.add(new Classes("Test Class 2", (float) 87.9));
+        classes.add(new Classes("Test Class 3", (float) 83.4));
+        classes.add(new Classes("Test Class 4", (float) 92.9));
+        classes.add(new Classes("Test Class 5", (float) 79.1));
+
+        ClassesRecViewAdapter adapter = new ClassesRecViewAdapter(this);
+        adapter.setClasses(classes);
+
+        classes_recView.setAdapter(adapter);
+        classes_recView.setLayoutManager(new LinearLayoutManager(this));
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
